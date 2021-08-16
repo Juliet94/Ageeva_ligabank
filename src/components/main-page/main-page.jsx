17f4f0ead696252c6317ab5c037d347d9b989c1b
Anastasia <Juliet94@users.noreply.github.com>
2021-08-16 04:14:23 +0300

    Добавит блок promo. WIP

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-
 import Header from '../header/header';
+import styles from './main-page.module.scss'
 
 function MainPage() {
   return (
     <Header />
+	<main>
+	<h1 className={visually-hidden}>
+		Лига банк - конвертер валют
+	</h1>
+	<div className={styles.promo}>
+		<div className={styles.wrapper}>
+			<h2 className={styles.heading}>
+				Лига Банк
+			<h2>
+			<p className={styles.slogan}>
+				Кредиты на любой случай
+			</p>
+			<a className={styles.link}>
+				Рассчитать кредит
+			</a>
+		</div>
+	</div>
+	</main>
   );
 }
 
